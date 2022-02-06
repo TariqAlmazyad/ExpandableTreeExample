0c3446e85a52dafa23d5d031b8633aa03c9d3859
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2022-02-06 12:11:20 +0300

    Initial Commit

---
+++ Shared/DemoSimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSimpleAppApp.swift
+//  Shared
+//
+//  Created by Tariq Almazyad on 2/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DemoSimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
